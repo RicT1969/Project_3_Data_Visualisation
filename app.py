025e42302e280dcb4c0f3bf7396b2851eb510dc6
Micah Raquena <micahtr@gmail.com>
2023-06-20 16:29:45 +1000

    newmodel:

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import numpy as np
-
 import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify, request, render_template
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, or_
+from sqlalchemy.ext.automap import automap_base
+
+from flask import (
+    Flask, 
+    jsonify, 
+    request, 
+    render_template, 
+    redirect)
+
+import pickle
 
 
 #################################################
@@ -15,10 +22,13 @@ engine = create_engine("sqlite:///quake_MMI_data.db")
 
 # reflect an existing database into a new model
 Base = automap_base()
+
 # reflect the tables
-Base.prepare(autoload_with=engine)
+Base.prepare(autoload_with=engine, reflect=True)
 
 # Save reference to the table
+print(Base.classes.keys())
+
 quake = Base.classes.quake_MMI_data
 
 #################################################
@@ -30,40 +40,30 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+# main html index
 
 @app.route("/")
 def main():
     return render_template("index.html")
 
-# @app.route("/data")
-# def data():
 
-#     return render_template("data.html")
+# ------------------------------------------------
+# API 
+# ------------------------------------------------
+# Data table
 
-# @app.route("/analysis")
-# def actors():
-#     return render_template("analysis.html")
-
-
-@app.route("/api/v1.0/intense")
-def mmiintense():
-    
+@app.route("/api/v1.0/quake_MMI_data")
+def quake_MMI_data():
     session = Session(engine)
 
-    results = session.query(quake.publicID, quake.date, quake.mmi, quake.magnitude).all()
+    results = session.query(quake.publicID, quake.date, quake.mmi, quake.magnitude, quake.longitude, quake.latitude, quake.time, quake.depth).all()
 
     session.close()
+    # Convert list of tuples into normal list
 
-    earthquake = [list(r) for r in results]
-
-    earthquake_history = {
-        "History": earthquake
-    }
-
-    return jsonify(earthquake_history)
+    earthquake = list(np.ravel(results))
+    return jsonify(earthquake)
     
-
-  
 
 if __name__ == '__main__':
     app.run(debug=True)
