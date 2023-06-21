07c77cd167c9497550b7de7e055dad339a4a7c78
Richard Thornton <richardethornton1969@gmail.com>
2023-06-22 00:28:35 +1000

    bar_chart_code

--- static/js/BarChart.js
+++ static/js/BarChart.js
@@ -1,16 +1,18 @@
 // Get the canvas element
 const barChartCanvas = document.getElementById('barChart');
-
+let allData = []; // Store all earthquake data
 // Fetch data from Flask endpoint
 fetch('/api/data')
   .then(response => response.json())
   .then(data => {
+    allData = data; // Store all data for filtering later
     // Extract labels and values from the data
     const labels = data.map(row => row.mmi);
     const values = data.map(row => row.depth);
-    
+    const dates = data.map(entry => entry.date);
+
     // Create the bar chart
-    new Chart(barChartCanvas, {
+    const chart = new chart(barChartCanvas, {
       type: 'bar',
       data: {
         labels: labels,
@@ -24,6 +26,19 @@ fetch('/api/data')
       },
       options: {
         scales: {
+          x: {
+            type: 'time',
+            time: {
+              unit: 'day',
+              displayFormats: {
+                day: 'MMM DD, YYYY'
+              }
+            },
+            ticks: {
+              autoSkip: true,
+              maxTicksLimit: 10
+            }
+          },
           y: {
             beginAtZero: true
           }
@@ -33,3 +48,24 @@ fetch('/api/data')
     console.log(data)
   });
 
+  // Event listener for button click
+document.getElementById('filterButton').addEventListener('click', updateChart);
+
+function updateChart() {
+  const dateFrom = document.getElementById('startDate').value;
+  const dateTo = document.getElementById('endDate').value;
+
+  // Filter the data based on the selected date range
+  const filteredData = allData.filter(entry => {
+    const entryDate = new Date(entry.date);
+    return entryDate >= new Date(dateFrom) && entryDate <= new Date(dateTo);
+  });
+
+  // Update the chart labels and data
+  const filteredLabels = filteredData.map(entry => entry.mmi);
+  const filteredValues = filteredData.map(entry => entry.depth);
+
+  chart.data.labels = filteredLabels;
+  chart.data.datasets[0].data = filteredValues;
+  chart.update();
+}
