906d947bd55ce3d2515e3b1777550bff1e583841
rsamma1606 <rsamma1606@gmail.com>
2023-06-21 21:30:13 +1000

    BarChart Creation

---
+++ static/js/BarChart.js
@@ -0,0 +1,33 @@
+// Get the canvas element
+const barChartCanvas = document.getElementById('barChart');
+
+// Fetch data from Flask endpoint
+fetch('/api/data')
+  .then(response => response.json())
+  .then(data => {
+    // Extract labels and values from the data
+    const labels = data.map(row => row.row);
+    const values = data.map(row => row.data);
+
+    // Create the bar chart
+    new Chart(barChartCanvas, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Bar Chart',
+          data: values,
+          backgroundColor: 'rgba(54, 162, 235, 0.5)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
