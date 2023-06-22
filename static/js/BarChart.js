a3e4f49bec3090546b2c03dd70cb2f4ca30c6f2f
Richard Thornton <richardethornton1969@gmail.com>
2023-06-22 17:17:51 +1000

    final charts

--- static/js/BarChart.js
+++ static/js/BarChart.js
@@ -1,71 +1,68 @@
-// Get the canvas element
-const barChartCanvas = document.getElementById('barChart');
-let allData = []; // Store all earthquake data
-// Fetch data from Flask endpoint
-fetch('/api/data')
-  .then(response => response.json())
-  .then(data => {
-    allData = data; // Store all data for filtering later
-    // Extract labels and values from the data
-    const labels = data.map(row => row.mmi);
-    const values = data.map(row => row.depth);
-    const dates = data.map(entry => entry.date);
+// Wait for the DOM content to load
+document.addEventListener('DOMContentLoaded', () => {
+  const barChartCanvas = document.getElementById('barChartCanvas');
+  let myChart; // Declare the chart variable
+  let originalData; // Store the original data for reference
 
-    // Create the bar chart
-    const chart = new chart(barChartCanvas, {
-      type: 'bar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Bar Chart',
-          data: values,
-          backgroundColor: 'rgba(54, 162, 235, 0.5)',
-          borderColor: 'rgba(54, 162, 235, 1)',
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          x: {
-            type: 'time',
-            time: {
-              unit: 'day',
-              displayFormats: {
-                day: 'MMM DD, YYYY'
-              }
-            },
-            ticks: {
-              autoSkip: true,
-              maxTicksLimit: 10
+  // Fetch data from Flask endpoint
+  fetch('/api/data')
+    .then(response => response.json())
+    .then(data => {
+      // Store the original data
+      originalData = data;
+
+      // Extract labels and values from the data
+      const labels = data.map(row => row.locality);
+      const values = data.map(row => row.magnitude);
+
+      // Create the bar chart and assign it to the chart variable
+      myChart = new Chart(barChartCanvas, {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: 'Bar Chart',
+            data: values,
+            backgroundColor: 'rgba(54, 162, 235, 0.5)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
             }
-          },
-          y: {
-            beginAtZero: true
           }
         }
-      }
+      });
+
+      console.log(data);
+
+      // Event listener for button click
+      document.getElementById('filterButton').addEventListener('click', updateChart);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
     });
-    console.log(data)
-  });
 
-  // Event listener for button click
-document.getElementById('filterButton').addEventListener('click', updateChart);
+  function updateChart() {
+    const dateFrom = document.getElementById('startDate').value;
+    const dateTo = document.getElementById('endDate').value;
 
-function updateChart() {
-  const dateFrom = document.getElementById('startDate').value;
-  const dateTo = document.getElementById('endDate').value;
+    // Filter the data based on the selected date range
+    const filteredData = originalData.filter(entry => {
+      const entryDate = new Date(entry.date);
+      return entryDate >= new Date(dateFrom) && entryDate <= new Date(dateTo);
+    });
 
-  // Filter the data based on the selected date range
-  const filteredData = allData.filter(entry => {
-    const entryDate = new Date(entry.date);
-    return entryDate >= new Date(dateFrom) && entryDate <= new Date(dateTo);
-  });
+    // Extract labels and values from the filtered data
+    const labels = filteredData.map(row => row.locality);
+    const values = filteredData.map(row => row.magnitude);
 
-  // Update the chart labels and data
-  const filteredLabels = filteredData.map(entry => entry.mmi);
-  const filteredValues = filteredData.map(entry => entry.depth);
-
-  chart.data.labels = filteredLabels;
-  chart.data.datasets[0].data = filteredValues;
-  chart.update();
-}
+    // Update the chart labels and data
+    myChart.data.labels = labels;
+    myChart.data.datasets[0].data = values;
+    myChart.update();
+  }
+});
